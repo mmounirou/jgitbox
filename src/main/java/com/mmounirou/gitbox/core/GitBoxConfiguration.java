0b05e6aa441cd72e4c760e224301cf8b1bcb7735
mmounirou <mmounirou@mmounirou.com>
2012-04-14 16:59:10 +0200

    add the possibility to use a default configuration file

--- src/main/java/com/mmounirou/gitbox/core/GitBoxConfiguration.java
+++ src/main/java/com/mmounirou/gitbox/core/GitBoxConfiguration.java
@@ -1,12 +1,16 @@
 package com.mmounirou.gitbox.core;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.annotation.Nonnull;
+import javax.swing.filechooser.FileSystemView;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+
+import com.google.common.io.Files;
 
 public class GitBoxConfiguration
 {
@@ -18,6 +22,34 @@ public class GitBoxConfiguration
 		configuration = new PropertiesConfiguration(configurationFile);
 		configuration.setAutoSave(true);
 		configuration.setReloadingStrategy(new FileChangedReloadingStrategy());
+	}
+
+	public GitBoxConfiguration() throws ConfigurationException
+	{
+		this(getGitBoxPropertyFile());
+	}
+
+	private static File getGitBoxPropertyFile() throws ConfigurationException
+	{
+		try
+		{
+
+			File homeDirectory = FileSystemView.getFileSystemView().getHomeDirectory();
+			File gitBoxDirectory = new File(homeDirectory, ".gitbox");
+			gitBoxDirectory.mkdirs();
+
+			File gitBoxPropertyFile = new File(homeDirectory, "gitbox.properties");
+			if (!gitBoxDirectory.exists())
+			{
+				Files.touch(gitBoxPropertyFile);
+			}
+			return gitBoxPropertyFile;
+
+		} catch (IOException e)
+		{
+			throw new ConfigurationException(e);
+		}
+
 	}
 
 	public long getDelayForLocalChangeCheckInSeconds()
