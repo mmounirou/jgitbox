7647f9e5232cc138e856a73e3197b7c5d91dca5c
mmounirou <mmounirou@mmounirou.com>
2012-04-15 12:46:32 +0200

    create installer

---
+++ src/izpack/startup.sh
@@ -0,0 +1,1 @@
+java -cp "${INSTALL_PATH}/gitbox-${gitbox-version}.jar":"${INSTALL_PATH}/libs/*" com.mmounirou.gitbox.main.Main --work-tree "${worktree}" --properties "${INSTALL_PATH}/config/gitbox.properties"
